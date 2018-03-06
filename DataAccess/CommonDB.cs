fb4cfed7b0cf430f245167346d702d05e6e76e38
TheReaper666 <jack0810@edu.campusvejle.dk>
2018-03-06 11:15:23 +0100

    Classes Created

--- DataAccess/CommonDB.cs
+++ DataAccess/CommonDB.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities
+namespace DataAccess
 {
-    public class Class1
+    public class CommonDB
     {
     }
 }
---
+++ Entities/Rentee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Rentee
+    {
+        private string name;
+        private string address;
+        private string phoneNumber;
+        private DateTime registerDate;
+
+        public DateTime RegisterDate
+        {
+            get { return registerDate; }
+            set { registerDate = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
